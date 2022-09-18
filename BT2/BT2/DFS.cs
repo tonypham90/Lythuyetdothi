3bca597b545976db9acd6638b5485a9e3f6a1ace
tonypham90 <tuananh.gt@hotmail.com>
2022-09-18 07:05:28 +0700

    Dang sua ConvertMatrixToAdjacencylist() trong file AdjacencyMatrix.cs

---
+++ BT2/BT2/DFS.cs
@@ -0,0 +1,20 @@
+namespace BT2;
+
+public class DFS
+{
+    private int[] Visited;
+    private int Start;
+    private int Goal;
+    private Dictionary<int, int[]> Adjacency_list;
+    private Dictionary<int, int> parent;
+
+
+    // public DFS(AdjacencyMatrix g)
+    // {
+    //     this.Start = g.Start;
+    //     Goal = g.Goal;
+    //     Adjacency_list = g.Adjacency_list;
+    // }
+}
+
+    
