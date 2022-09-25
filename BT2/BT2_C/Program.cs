ad68add37ba63869d489c51d123ba4f09138f478
tonypham90 <tuananh.gt@hotmail.com>
2022-09-25 14:49:04 +0700

    complete

--- BT2/BT2_C/Program.cs
+++ BT2/BT2_C/Program.cs
@@ -10,8 +10,8 @@ public static class Program
                           "\n - Dòng 2 là cạnh và đích tuy nhiên sẽ không có giá trị vì bài C xét liên thông" +
                           "\n - Các dòng tiếp theo là ma trận biểu diễn quan hệ kết nối các đỉnh\n");
         Console.Write("Nhập đường dẫn tuyệt đối (bao gồm file) đến file chứa dữ liệu: ");
-        // var path = Console.ReadLine();
-        Graph.readAdjacencyMatrix("/Users/tonypham/Documents/GitHub/Lythuyetdothi/BT2/BT2_C/Input/VD6.txt");
+        var path = Console.ReadLine();
+        Graph.readAdjacencyMatrix(path);
         var lienthong = new ConnectedComponent(Graph);
 
     }
